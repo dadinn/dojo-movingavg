0372d0fcfc99d7f5cdb10349893974bb89a2dc87
Daniel Dinnyes <ddinnyes@gmail.com>
2017-01-31 19:50:39 +0000

    initial commit

---
+++ test/dojo_movingavg/core_test.clj
@@ -0,0 +1,7 @@
+(ns dojo-movingavg.core-test
+  (:require [clojure.test :refer :all]
+            [dojo-movingavg.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
